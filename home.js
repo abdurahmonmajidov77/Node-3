b63df69fc1734754175830fe40996746ddac671e
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-11 16:13:53 +0500

    gh

--- home.js
+++ home.js
@@ -2,6 +2,6 @@ const http = require('http')
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('./index.html')
+    res.write('')
     res.end()
 }).listen(8010)
